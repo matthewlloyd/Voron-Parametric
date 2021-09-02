64172c2fb313c2d87e892146e72c82e21e6fa09a
github@matthewlloyd.net <github@matthewlloyd.net>
2021-09-02 11:55:46 -0400

    Add parametric version of Voron logo, and add outer hexagon

--- assets/voron_logo.py
+++ assets/voron_logo.py
@@ -1,43 +1,28 @@
 #!/usr/bin/python3
 
 import drawSvg as draw
+import itertools
 
 
+# scaled so overall height is 1, and logo is centered on (0, 0)
 polys = [
-    [(103.699, -82.118), (107.07, -82.118), (102.46, -89.131), (99.09, -89.131)],
-    [(109.909, -82.118), (113.279, -82.118), (104.06, -96.142), (100.69, -96.142)],
-    [(111.508, -89.131), (114.878, -89.131), (110.269, -96.142), (106.899, -96.142)]
+    # outer hexagon
+    ['red', (-0.433, 0.25), (0, 0.5), (0.433, 0.25), (0.433, -0.25), (0, -0.5), (-0.433, -0.25)],
+
+    # left
+    [None, (-0.312, 0), (-0.153, .276), (-0.028, 0.276), (-0.187, 0), (-0.312, 0)],
+    # middle
+    [None, (0.097, 0.276), (0.222, 0.276), (-0.097, -0.276), (-0.222, -0.276)],
+    # right
+    [None, (0.187, 0), (0.312, 0), (0.153, -0.276), (0.028, -0.276)]
 ]
 
-min_x = min([p[0] for poly in polys for p in poly])
-min_y = min([p[1] for poly in polys for p in poly])
-max_x = max([p[0] for poly in polys for p in poly])
-max_y = max([p[1] for poly in polys for p in poly])
-
-print(f'min_x {min_x}')
-print(f'min_y {min_y}')
-print(f'max_x {max_x}')
-print(f'max_y {max_y}')
-print(f'width {max_x - min_x}')
-print(f'height {max_y - min_y}')
-
-scale = 1 / max( (max_y - min_y), (max_x - min_x) )
-center_x = (min_x + max_x) / 2.
-center_y = (min_y + max_y) / 2.
-
-polys = [[((p[0] - center_x) * scale, (p[1] - center_y) * scale) for p in poly] for poly in polys]
-
-for poly in polys:
-    print(':'.join([f'{p[0]:.3f},{p[1]:.3f}' for p in poly]))
-
-svg_scale = 1000
+svg_scale = 1000.
 d = draw.Drawing(svg_scale, svg_scale, origin='center', displayInline=False)
 
 for poly in polys:
-    pts = []
-    for p in poly:
-        pts.append(p[0] * svg_scale)
-        pts.append(p[1] * svg_scale)
-    d.append(draw.Lines(*pts, close=True, stroke='black'))
+    d.append(draw.Lines(
+        *itertools.chain(*[(p[0] * svg_scale, p[1] * svg_scale) for p in poly[1:]]),
+        close=True, stroke=poly[0], fill=poly[0]))
 
 d.saveSvg('voron_logo.svg')
